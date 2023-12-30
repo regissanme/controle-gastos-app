db216708c9d0e07e0a9e2437b7f7ff55c25cd47b
regissanme <rsanme@gmail.com>
2023-12-30 20:43:08 -0300

    chore: remove placeholder in app component after creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'controle-gastos-app';
+  title = 'Controle de Gastos App';
 }
