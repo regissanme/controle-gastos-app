87ae2fc7b143b83220e3c78a5f113366f174dfd3
regissanme <rsanme@gmail.com>
2024-01-04 23:07:52 -0300

    chore: change login api url

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const AUTH_API = "http://localhost:8080/api/v1/";
+const AUTH_API = "http://localhost:8080/api/v1/login";
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -18,11 +18,12 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(username: string, password: string): Observable<any> {
+    console.log("login(): user = " + username + " pwd = " + password);
     return this.http.post(
-      AUTH_API + 'login',
+      AUTH_API,
       {
-        'usernme': username,
-        'password': password
+        "username": username,
+        "password": password
       },
       httpOptions
     );
