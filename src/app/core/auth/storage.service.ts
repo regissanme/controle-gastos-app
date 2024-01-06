44426051c578856e4221d04aa680bb4c2f9034b5
regissanme <rsanme@gmail.com>
2024-01-06 13:20:32 -0300

    feature: create user storage service

---
+++ src/app/core/auth/storage.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { User } from '../../components/shared/models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  user!: User;
+
+  constructor() { }
+
+  setUser(user: User) {
+    this.user = user;
+  }
+
+  getUser(): User {
+    return this.user;
+  }
+
+  isLoggedIn(): boolean {
+    if (this.user) {
+      return true;
+    }
+    return false;
+  }
+}
