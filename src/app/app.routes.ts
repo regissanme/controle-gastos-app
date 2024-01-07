a162217d1ef82e8c068caa60a61d624ef2e2180f
regissanme <rsanme@gmail.com>
2024-01-06 21:47:15 -0300

    chore: add register route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,6 @@ export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent) },
-  { path: '**', loadComponent: () => import('./page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent) }
+  { path: 'register', loadComponent: () => import('./components/register/register.component').then(m => m.RegisterComponent) },
+    { path: '**', loadComponent: () => import('./page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent) }
 ];
