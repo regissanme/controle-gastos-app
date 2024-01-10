e1bf75a7f861b917a5d652f9c13df0545d45c131
regissanme <rsanme@gmail.com>
2024-01-09 23:41:40 -0300

    feature: create dashboard page and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,5 +6,6 @@ export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent) },
   { path: 'register', loadComponent: () => import('./components/register/register.component').then(m => m.RegisterComponent) },
+  { path: 'dashboard', loadComponent: () => import('./components/dashboard/dashboard.component').then(m => m.DashboardComponent) },
     { path: '**', loadComponent: () => import('./page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent) }
 ];
