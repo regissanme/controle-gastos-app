4669ca7a7a53de93d7da5aa508b960aa03b304ce
regissanme <rsanme@gmail.com>
2024-01-11 20:44:48 -0300

    chore: add lazy loading for private routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,9 @@ export const routes: Routes = [
   { path: 'home', component: HomePageComponent },
   { path: 'login', loadComponent: () => import('./core/login/login.component').then(m => m.LoginComponent) },
   { path: 'register', loadComponent: () => import('./core/register/register.component').then(m => m.RegisterComponent) },
-  { path: 'dashboard', loadComponent: () => import('./domain/components/dashboard/dashboard.component').then(m => m.DashboardComponent) },
+  {
+    path: 'app',
+    loadChildren: () => import('./domain/domain.routes').then(r => r.DOMAIN_ROUTES)
+  },
   { path: '**', loadComponent: () => import('./public/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent) }
 ];
---
+++ src/app/domain/domain.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+
+
+export const DOMAIN_ROUTES: Routes = [
+  // { path: '', component: HomeComponent },
+  // { path: 'dashboard', loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent) },
+  {
+    path: '',
+    component: HomeComponent, // this is the component with the <router-outlet> in the template
+    children: [
+      { path: '',   redirectTo: '/app/dashboard', pathMatch: 'full' },
+      { path: 'dashboard', loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent) },
+
+    ],
+  },
+];
