18ff1fb42c5fcc3648074275de13b85c07d3a471
regissanme <rsanme@gmail.com>
2024-01-11 23:21:09 -0300

    feature: create a custom sidenav and items

--- src/app/domain/components/custom-sidenav/custom-sidenav.component.ts
+++ src/app/domain/components/custom-sidenav/custom-sidenav.component.ts
@@ -2,6 +2,10 @@ import { CommonModule } from '@angular/common';
 import { Component, Input, computed, signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { User } from '../../../shared/models/user';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 export type MenuItem = {
   icon: string;
@@ -13,18 +17,31 @@ export type MenuItem = {
   selector: 'app-custom-sidenav',
   standalone: true,
   imports: [
-    CommonModule,
-    MatListModule, MatIconModule
+    CommonModule, RouterLink, RouterLinkActive,
+    MatListModule, MatIconModule, MatButtonModule, MatTooltipModule
   ],
   templateUrl: './custom-sidenav.component.html',
   styleUrl: './custom-sidenav.component.css'
 })
 export class CustomSidenavComponent {
 
+  user: User = {
+    name: 'Reginaldo Santos de Medeiros',
+    active: true,
+    username: 'teste@email.com',
+    birthdate: '1978-11-14',
+    role: 'Admin',
+    lastAccessAt: '',
+    token: '',
+    id: ''
+  };
   sidenavCollapsed = signal(false);
 
   @Input() set collapsed(val: boolean) {
     this.sidenavCollapsed.set(val);
+  }
+  @Input() setUser(user: User) {
+    this.user = user;
   }
 
   menuItems = signal<MenuItem[]>([
@@ -47,5 +64,9 @@ export class CustomSidenavComponent {
   ]);
 
   profilePicSize = computed(() => this.sidenavCollapsed() ? '32' : '100');
+
+  getSymbol(): string {
+    return this.user ? this.user.name.toLocaleUpperCase().charAt(0) : 'R';
+  }
 
 }
