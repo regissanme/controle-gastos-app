4d916cbecfb1b2309709803e57a30cddd3df02d3
regissanme <rsanme@gmail.com>
2024-01-11 23:22:14 -0300

    refactor: change user class to interface

--- src/app/shared/models/user.ts
+++ src/app/shared/models/user.ts
@@ -1,32 +1,12 @@
-export class User {
-  id: string = '';
-  username: string = '';
-  name: string = '';
-  role: string = '';
-  active: boolean = false;
-  birthdate: string = '';
-  lastAccessAt: string = '';
-  token: string = '';
-
-  contructor(
-    id: string,
-    username: string,
-    name: string,
-    role: string,
-    active: boolean,
-    birthdate: string,
-    lastAccessAt: string,
-    token: string
-  ) {
-    this.id = id;
-    this.username = username;
-    this.name = name;
-    this.role = role;
-    this.active = active;
-    this.birthdate = birthdate;
-    this.lastAccessAt = lastAccessAt;
-    this.token = token;
-  }
+export interface User {
+  id: string;
+  username: string;
+  name: string;
+  role: string;
+  active: boolean;
+  birthdate: string;
+  lastAccessAt: string;
+  token: string;
 }
 
 
