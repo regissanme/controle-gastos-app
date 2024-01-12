3ed5d151acdac1b4e86322612e39a84557943638
regissanme <rsanme@gmail.com>
2024-01-12 18:55:49 -0300

    layout: transfer component elements to home component

--- src/app/domain/pages/dashboard/dashboard.component.ts
+++ src/app/domain/pages/dashboard/dashboard.component.ts
@@ -1,44 +1,16 @@
-import { Component, OnInit, computed, signal } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { Router, RouterOutlet } from '@angular/router';
-import { StorageService } from '../../../core/storage/storage.service';
-import { User } from '../../../shared/models/user';
-import { CustomSidenavComponent } from '../custom-sidenav/custom-sidenav.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
-    RouterOutlet,
-    CustomSidenavComponent,
-    MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule
+
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent implements OnInit {
-
-  collapsed = signal(false);
-  sidenavWidth = computed(() => this.collapsed() ? '65px' : '200px');
-
-  user?: User;
-
-  constructor(
-    private storageService: StorageService,
-    private router: Router
-  ) { }
+export class DashboardComponent {
 
 
-  ngOnInit(): void {
-    console.log('dashboard on init - logged in: ', this.storageService.isLoggedIn());
-    if (!this.storageService.isLoggedIn()) {
-      // this.router.navigate(['/login']);
-    } else {
-      this.user = this.storageService.getUser();
-    }
-  }
 
 }
