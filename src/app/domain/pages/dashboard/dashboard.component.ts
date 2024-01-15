b7237877fee5f952fdf1442ac3466055317ab9b6
regissanme <rsanme@gmail.com>
2024-01-15 18:54:46 -0300

    layout(dashboard): add cards components

--- src/app/domain/pages/dashboard/dashboard.component.ts
+++ src/app/domain/pages/dashboard/dashboard.component.ts
@@ -1,16 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { of } from 'rxjs';
+import { DashboardCardComponent } from '../../components/dashboard-card/dashboard-card.component';
 
 @Component({
   selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
   standalone: true,
   imports: [
-
-  ],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+    CommonModule,
+    AsyncPipe,
+    MatGridListModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    DashboardCardComponent
+  ]
 })
 export class DashboardComponent {
+  private breakpointObserver = inject(BreakpointObserver);
 
+cardLayout = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+   map(({ matches }) => {
+     if (matches) {
+       return {
+         columns: 1,
+         miniCard: { cols: 1, rows: 1 },
+         chart: { cols: 1, rows: 2 },
+         table: { cols: 1, rows: 4 },
+       };
+     }
 
+    return {
+       columns: 4,
+       miniCard: { cols: 1, rows: 1 },
+       chart: { cols: 2, rows: 2 },
+       table: { cols: 4, rows: 4 },
+     };
+   })
+ );
 
 }
