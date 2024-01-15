6ef13b9bc30e5e7cdf74bf73dae05a7b6ce84cff
regissanme <rsanme@gmail.com>
2024-01-15 20:43:30 -0300

    layout: update dashboard with monthly chart component

--- src/app/domain/pages/dashboard/dashboard.component.ts
+++ src/app/domain/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,13 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
-import { map } from 'rxjs/operators';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { AsyncPipe, CommonModule } from '@angular/common';
-import { of } from 'rxjs';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { map } from 'rxjs/operators';
+import { MonthlyCharComponent } from '../../charts/monthly-char/monthly-char.component';
 import { DashboardCardComponent } from '../../components/dashboard-card/dashboard-card.component';
 
 @Component({
@@ -23,30 +23,31 @@ import { DashboardCardComponent } from '../../components/dashboard-card/dashboar
     MatIconModule,
     MatButtonModule,
     MatCardModule,
-    DashboardCardComponent
+    DashboardCardComponent,
+    MonthlyCharComponent
   ]
 })
 export class DashboardComponent {
   private breakpointObserver = inject(BreakpointObserver);
 
-cardLayout = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-   map(({ matches }) => {
-     if (matches) {
-       return {
-         columns: 1,
-         miniCard: { cols: 1, rows: 1 },
-         chart: { cols: 1, rows: 2 },
-         table: { cols: 1, rows: 4 },
-       };
-     }
+  cardLayout = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return {
+          columns: 1,
+          miniCard: { cols: 1, rows: 1 },
+          chart: { cols: 1, rows: 2 },
+          table: { cols: 1, rows: 4 },
+        };
+      }
 
-    return {
-       columns: 4,
-       miniCard: { cols: 1, rows: 1 },
-       chart: { cols: 2, rows: 2 },
-       table: { cols: 4, rows: 4 },
-     };
-   })
- );
+      return {
+        columns: 4,
+        miniCard: { cols: 1, rows: 1 },
+        chart: { cols: 2, rows: 2 },
+        table: { cols: 4, rows: 4 },
+      };
+    })
+  );
 
 }
