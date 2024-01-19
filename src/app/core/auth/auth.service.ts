8d218ea3743e6e14e080e4b1e31caec0a2232020
regissanme <rsanme@gmail.com>
2024-01-19 13:39:16 -0300

    feature: adjust auth service to handle user logged data

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit, computed, signal } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { User } from '../../shared/models/user';
 
 const AUTH_API = "http://localhost:8080/api/v1/login";
 
@@ -11,11 +13,44 @@ const httpOptions = {
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
+export class AuthService implements OnInit {
 
+  currentUserSig = signal<User | undefined | null>(undefined);
+  isLoggedIn = computed(() => this.currentUserSig() ? true : false);
 
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
 
-  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  setUser(user: User) {
+    localStorage.setItem('token', user.token);
+    localStorage.setItem('user', JSON.stringify(user));
+    this.currentUserSig.set(user);
+  }
+
+  getUser() {
+    // JSON.parse(localStorage.getItem('user'))
+
+    let storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      this.currentUserSig.set(JSON.parse(storedUser));
+      return JSON.parse(storedUser);
+    }
+    this.currentUserSig.set(null);
+    return null;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    this.currentUserSig.set(null);
+    this.router.navigate(['/']);
+  }
 
   login(username: string, password: string): Observable<any> {
     console.log("login(): user = " + username + " pwd = " + password);
