043792a62aceabc597b04d6a8a8eb54e2f3adc20
regissanme <rsanme@gmail.com>
2024-01-22 13:43:03 -0300

    chore: update dashboard components with expanse service

--- src/app/domain/pages/dashboard/dashboard.component.ts
+++ src/app/domain/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { AsyncPipe, CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
@@ -10,7 +10,8 @@ import { map } from 'rxjs/operators';
 import { MonthlyChartComponent } from '../../charts/monthly-chart/monthly-chart.component';
 import { DashboardCardComponent } from '../../components/dashboard-card/dashboard-card.component';
 import { DashboardHeaderCardComponent } from '../../components/dashboard-header-card/dashboard-header-card.component';
-import { ExpensesTableComponent } from '../../components/expenses-table/expenses-table.component';
+import { CardHeaderData } from '../../models/card-header-data';
+import { ExpensesService } from './../../services/expenses.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -27,12 +28,48 @@ import { ExpensesTableComponent } from '../../components/expenses-table/expenses
 
     DashboardCardComponent,
     DashboardHeaderCardComponent,
-    ExpensesTableComponent,
     MonthlyChartComponent,
   ]
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  constructor(
+    private expensesService: ExpensesService
+  ) { }
+
+  ngOnInit(): void {
+    this.expensesService.getAll().subscribe({
+      next: data => {
+        console.log(data);
+      }
+    })
+  }
+
   private breakpointObserver = inject(BreakpointObserver);
+
+  totals = signal<CardHeaderData[]>([
+    {
+      type: 'receitas',
+      value: '9900.00',
+      route: '/receitas',
+      new: 'Nova Receita',
+      tip: 'Ir para Receitas'
+    },
+    {
+      type: 'despesas',
+      value: '7500.00',
+      route: '/despesas',
+      new: 'Nova Despesa',
+      tip: 'Ir para Despesas'
+    },
+    {
+      type: 'saldo',
+      value: '2400.00',
+      route: '/saldo',
+      new: '',
+      tip: 'Ir para o Saldo'
+    }
+  ]);
 
   cardLayout = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
@@ -46,12 +83,34 @@ export class DashboardComponent {
       }
 
       return {
-        columns: 4,
-        miniCard: { cols: 1, rows: 1 },
-        chart: { cols: 2, rows: 2 },
-        table: { cols: 4, rows: 4 },
+        columns: 12,
+        miniCard: { cols: 4, rows: 1 },
+        chart: { cols: 6, rows: 2 },
+        table: { cols: 12, rows: 4 },
       };
     })
   );
+
+
+
+  // cardLayout = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+  //   map(({ matches }) => {
+  //     if (matches) {
+  //       return {
+  //         columns: 1,
+  //         miniCard: { cols: 1, rows: 1 },
+  //         chart: { cols: 1, rows: 2 },
+  //         table: { cols: 1, rows: 4 },
+  //       };
+  //     }
+
+  //     return {
+  //       columns: 4,
+  //       miniCard: { cols: 1, rows: 1 },
+  //       chart: { cols: 2, rows: 2 },
+  //       table: { cols: 4, rows: 4 },
+  //     };
+  //   })
+  // );
 
 }
