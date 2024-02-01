b7fcdf040b6508ba5b3fdc251ebe219f4da36b93
regissanme <rsanme@gmail.com>
2024-02-01 15:26:55 -0300

    layout: add style for mat button toggle

--- src/app/domain/components/month-selector/month-selector.component.ts
+++ src/app/domain/components/month-selector/month-selector.component.ts
@@ -7,6 +7,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 export interface month {
   index: number;
@@ -18,7 +19,7 @@ export interface month {
   standalone: true,
   imports: [
     CommonModule, ReactiveFormsModule,
-    MatCardModule, MatIconModule, MatMenuModule, MatButtonModule, MatTooltipModule, MatButtonToggleModule
+    MatCardModule, MatIconModule, MatMenuModule, MatButtonModule, MatTooltipModule, MatButtonToggleModule, MatDatepickerModule
   ],
   templateUrl: './month-selector.component.html',
   styleUrl: './month-selector.component.css'
@@ -27,23 +28,28 @@ export class MonthSelectorComponent {
 
   @Output() monthEvent = new EventEmitter<number>();
 
-  months: month[]=[
-    { index: 0, name: "JAN" },
-    { index: 1, name: "FEV" },
-    { index: 2, name: "MAR" },
-    { index: 3, name: "ABR" },
-    { index: 4, name: "MAI" },
-    { index: 5, name: "JUN" },
-    { index: 6, name: "JUL" },
-    { index: 7, name: "AGO" },
-    { index: 8, name: "SET" },
-    { index: 9, name: "OUT" },
-    { index: 10, name: "NOV" },
-    { index: 11, name: "DEZ" },
+  months: month[][] = [
+    [
+      { index: 0, name: "JAN" },
+      { index: 1, name: "FEV" },
+      { index: 2, name: "MAR" },
+      { index: 3, name: "ABR" },
+      { index: 4, name: "MAI" },
+      { index: 5, name: "JUN" },
+    ],
+    [
+      { index: 6, name: "JUL" },
+      { index: 7, name: "AGO" },
+      { index: 8, name: "SET" },
+      { index: 9, name: "OUT" },
+      { index: 10, name: "NOV" },
+      { index: 11, name: "DEZ" },
+    ]
   ];
 
   monthControl = new FormControl(this.months[0]);
   selectedMonth = new Date("2024-11-14").getMonth();
+    selected: Date | null = new Date();
 
   selectMonth(val: number) {
     console.log("Emitindo Mês selecionado: ", val);
