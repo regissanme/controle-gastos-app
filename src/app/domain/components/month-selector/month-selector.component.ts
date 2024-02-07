42dc4aab22d922f4ff5b2d3ee1824ebcb971e549
regissanme <rsanme@gmail.com>
2024-02-06 23:31:32 -0300

    layout: change month selector style

--- src/app/domain/components/month-selector/month-selector.component.ts
+++ src/app/domain/components/month-selector/month-selector.component.ts
@@ -28,30 +28,26 @@ export class MonthSelectorComponent {
 
   @Output() monthEvent = new EventEmitter<number>();
 
-  months: month[][] = [
-    [
+  months: month[] = [
       { index: 0, name: "JAN" },
       { index: 1, name: "FEV" },
       { index: 2, name: "MAR" },
       { index: 3, name: "ABR" },
       { index: 4, name: "MAI" },
       { index: 5, name: "JUN" },
-    ],
-    [
       { index: 6, name: "JUL" },
       { index: 7, name: "AGO" },
       { index: 8, name: "SET" },
       { index: 9, name: "OUT" },
       { index: 10, name: "NOV" },
       { index: 11, name: "DEZ" },
-    ]
   ];
 
   monthControl = new FormControl(this.months[0]);
-  selectedMonth = new Date("2024-11-14").getMonth();
+  selectedMonth = new Date().getMonth();
     selected: Date | null = new Date();
 
-  selectMonth(val: number) {
+    selectMonth(val: number) {
     console.log("Emitindo Mês selecionado: ", val);
     this.selectedMonth = val;
     this.monthEvent.emit(val);
