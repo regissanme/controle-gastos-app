e7dd90ba1695cddbb6ef3fe25065d396b0279b97
regissanme <rsanme@gmail.com>
2024-02-27 14:15:57 -0300

    chore: remove dev logs

--- src/app/shared/interceptors/loader.interceptor.ts
+++ src/app/shared/interceptors/loader.interceptor.ts
@@ -1,6 +1,6 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { finalize, tap } from 'rxjs';
+import { finalize } from 'rxjs';
 import { LoaderService } from '../services/loader.service';
 
 export const loaderInterceptor: HttpInterceptorFn = (req, next) => {
@@ -9,7 +9,8 @@ export const loaderInterceptor: HttpInterceptorFn = (req, next) => {
   loaderService.isLoading$.next(true);
 
   return next(req).pipe(
-    tap(v => console.log("Initiated loading..............")),
-    finalize(() => { loaderService.isLoading$.next(false), console.log("Finalize loading.............") })
+    finalize(() => {
+      loaderService.isLoading$.next(false)
+    })
   );
 };
