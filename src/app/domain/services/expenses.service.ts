78fe22784778cbdf26641e5032831205102fe470
regissanme <rsanme@gmail.com>
2024-03-01 14:08:13 -0300

    chore: transform expenses data to chart data

--- src/app/domain/services/expenses.service.ts
+++ src/app/domain/services/expenses.service.ts
@@ -2,13 +2,18 @@ import { Injectable, computed, inject, signal } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Expense } from '../models/expense';
 import { ExpenseChartData } from '../models/expense-chart-data';
+import { ExpenseFilterType } from '../models/expense-filter-type';
+import { ExpenseCategoryService } from './expense-category.service';
 import { ExpensesApiService } from './expenses-api.service';
+import { PaymentService } from './payment.service';
 @Injectable({
   providedIn: 'root'
 })
 export class ExpensesService {
 
   private expensesApiService = inject(ExpensesApiService);
+  private paymentService = inject(PaymentService);
+  private categoryService = inject(ExpenseCategoryService);
 
   _expenses = new BehaviorSubject<Expense[]>([]);
   expensesSig = signal<Expense[]>([]);
@@ -26,7 +31,7 @@ export class ExpensesService {
     return this.expensesApiService.create(expense);
   }
 
-  getAllExpensesByYear(year: number) {
+  getAllExpensesByYear(year: number): void {
     this.expensesApiService.getAllExpensesByYear(year)
       .subscribe(value => {
         this._expenses.next(value),
@@ -34,7 +39,7 @@ export class ExpensesService {
       });
   }
 
-  getAllExpensesByMonth(month: number, year: number) {
+  getAllExpensesByMonth(month: number, year: number): void {
     this.expensesApiService.getAllExpensesByMonth(month, year)
       .subscribe(value => {
         this._expenses.next(value),
@@ -42,38 +47,64 @@ export class ExpensesService {
       });
   }
 
-  findYearsAndMonths(year: number) {
+  findYearsAndMonths(year: number): void {
     this.getYearsWithExpensesFromUser();
     this.getMonthsWithExpensesFromUserAndYear(year);
   }
 
-  getYearsWithExpensesFromUser() {
+  getYearsWithExpensesFromUser(): void {
     this.expensesApiService.getYearsWithExpensesFromUser()
       .subscribe(values => {
         this.yearsWithExpenses.set(values);
       });
   }
 
-  getMonthsWithExpensesFromUserAndYear(year: number) {
+  getMonthsWithExpensesFromUserAndYear(year: number): void {
     this.expensesApiService.getMonthsWithExpensesFromUserAndYear(year)
       .subscribe(values => {
         this.monthsWithExpenses.set(values);
       });
   }
 
-  getChartDataByType(): ExpenseChartData[] {
+  getChartDataByType(type: ExpenseFilterType): ExpenseChartData[] {
 
-    let response: ExpenseChartData[] = [];
-    [...this._expenses.getValue()].map(e =>
-      response.push({ label: `${e.tipoDespesaId}`, total: e.valor } as ExpenseChartData)
-    );
+    let response = this.expensesToChartData(type);
 
-    return [...response]?.reduce((acc: ExpenseChartData[], ele) => {
-      const existingExpense = acc.find(x => x.label === ele.label);
-      if (!existingExpense) return acc.concat(ele);
-      return (existingExpense.total += ele.total, acc);
+    return [...response]?.reduce((chartData: ExpenseChartData[], item) => {
+      const existingExpense = chartData.find(x => x.label === item.label);
+      if (!existingExpense) return chartData.concat(item);
+      return (existingExpense.total += item.total, chartData);
     }, []);
   }
 
+  private expensesToChartData(type: ExpenseFilterType): ExpenseChartData[] {
+    let response: ExpenseChartData[] = [];
+
+    if (type === ExpenseFilterType.Type) {
+      [...this._expenses.getValue()].map(e =>
+        response.push({ label: `${this.getTypeName(e.tipoDespesaId)}`, total: e.valor })
+      );
+    }
+
+    if (type === ExpenseFilterType.Payment) {
+      [...this._expenses.getValue()].map(e =>
+        response.push({ label: `${this.getPaymentName(e.tipoPagamentoId)}`, total: e.valor })
+      );
+    }
+
+    return response;
+  }
+
+  private getTypeName(id: number): string {
+    return this.categoryService.getExpenseTypeName(id);
+  }
+
+  private getCategoryName(id: number): string {
+    return this.categoryService.getExpenseCategoryName(id);
+  }
+
+  private getPaymentName(id: number): string {
+    return this.paymentService.getPaymentName(id);
+  }
 
 }
